c5bf3c11fcb7eb7bf6dfcb4b48253fd6c3c4556b
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-08-23 12:31:21 -0700

    create reducers/step_reducer.js; write stepReducer with switch for action.type and a default that returns state; initialize state as an empty object; export stepReducer

---
+++ frontend/reducers/step_reducer.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+const stepReducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+export default stepReducer;
